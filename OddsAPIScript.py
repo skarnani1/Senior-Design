679634346ffad103422bbfe5fb93b1c5c02e1fa1
arnavghatiwala <58335590+arnavghatiwala1@users.noreply.github.com>
2024-11-18 18:37:25 -0500

    odds_api work

--- OddsAPIScript.py
+++ OddsAPIScript.py
@@ -16,28 +16,6 @@ ODDS_FORMAT = 'american' # decimal | american
 
 DATE_FORMAT = 'iso' # iso | unix
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#
-# First get a list of in-season sports
-#   The sport 'key' from the response can be used to get odds in the next request
-#
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-# sports_response = requests.get(
-#     'https://api.the-odds-api.com/v4/sports', 
-#     params={
-#         'api_key': API_KEY
-#     }
-# )
-
-
-# if sports_response.status_code != 200:
-#     print(f'Failed to get sports: status_code {sports_response.status_code}, response body {sports_response.text}')
-
-# else:
-#     print('List of in season sports:', sports_response.json())
-
-
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #
