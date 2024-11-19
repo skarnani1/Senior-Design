3c80569eaccd15b3f970ad3c6681c662059e5141
Jason Figueroa <jasonfig@seas.upenn.edu>
2024-11-19 13:52:41 -0500

    Get percentage with arbitrage detected

--- arbitrageDetector.py
+++ arbitrageDetector.py
@@ -14,7 +14,7 @@ def arbitrageExists(odd1, odd2):
     else: 
         percentage2 = 100 / (100 + odd2)
     
-    return percentage1 + percentage2 < 1
+    return (1 - (percentage1 + percentage2), percentage1 + percentage2 < 1)
 
 # Specify the path to your CSV file
 csv_file_path = "./OddsAPI_combined_odds.csv"
@@ -27,8 +27,10 @@ for index1, row1 in df.iterrows():
         #Check that it is the same game and that you are on different sportsbooks
         if (row1[2] == row2[2] and index1 < index2 and row1[3] != row2[3] and row1[0] == row2[0] and row1[1] == row2[1] and row1[5] == row2[5] 
     and row1[7] == row2[7] and row1[6] != row2[6]):
-            if arbitrageExists(int(row1[4]), int(row2[4])):
+            percent, exists = arbitrageExists(int(row1[4]), int(row2[4]))
+            if exists:
                 print("Arbitrage detected")
+                print("Percent: ", percent)
                 print(row1)
                 print(row2)
 
